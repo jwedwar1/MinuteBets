c1d217231f638992784bc6ca7eb53f6b291ed9c0
jalong2 <43768152+jalong2@users.noreply.github.com>
2018-10-03 11:03:42 -0500

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AboutPage } from '../pages/about/about';
 import { SignInPage } from '../pages/signin/signin';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { QuestionPage } from '../pages/question/question';
+import { InGamePage } from '../pages/ingame/ingame';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,6 +19,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     SignInPage,
     HomePage,
+    QuestionPage,
+    InGamePage,
     TabsPage
   ],
   imports: [
@@ -29,6 +33,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     SignInPage,
     HomePage,
+    QuestionPage,
+    InGamePage,
     TabsPage
   ],
   providers: [
---
+++ src/pages/ingame/ingame.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-ingame',
+  templateUrl: 'ingame.html'
+})
+export class InGamePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
