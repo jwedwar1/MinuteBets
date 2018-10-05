aa268351556fe63f319bdc4b7a197fe0855d37be
jwedwar1 <jwedwar1@gmail.com>
2018-10-05 09:48:20 -0500

    Add files via upload

--- src/pages/ingame/ingame.ts
+++ src/pages/ingame/ingame.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { HomePage } from './../home/home';
 
 @Component({
   selector: 'page-ingame',
@@ -7,8 +9,26 @@ import { NavController } from 'ionic-angular';
 })
 export class InGamePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public alertCtrl: AlertController, public navCtrl: NavController) { }
 
+
+  goBack() {
+    	this.navCtrl.pop();
+  	}
+
+  goToHomePage() {
+    //push another page onto the history stack
+    //causing the nav controller to animate the new page in
+    this.navCtrl.push(HomePage);
+  }
+
+  doAccountAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Coming Soon!',
+      message: 'This button will link to a webpage, such as RebelRewards, where students can redeem their tokens for prizes',
+      buttons: ['Ok']
+    });
+    alert.present()
   }
 
 }
