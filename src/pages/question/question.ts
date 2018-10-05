84a10e2a4163177c164b75d815860347c1807a4d
jwedwar1 <jwedwar1@gmail.com>
2018-10-05 16:15:44 -0500

    Update question.ts

--- src/pages/question/question.ts
+++ src/pages/question/question.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-question',
@@ -7,8 +8,15 @@ import { NavController } from 'ionic-angular';
 })
 export class QuestionPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public alertCtrl: AlertController, public navCtrl: NavController) { }
 
+   submitAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Coming Soon!',
+      message: 'This button will send the input to the server, where the results will be calculated',
+      buttons: ['Ok']
+    });
+    alert.present()
   }
 
 }
